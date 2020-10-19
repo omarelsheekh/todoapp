9d0db556add3f4642bc7800fa9c9ab486d026a6a
OmarElsheekh <omarelsheekh@manager.omar.com>
2020-10-19 15:16:44 -0400

    add lists create & read

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import sys
 
+from sqlalchemy.orm import backref
+
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///db1'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
@@ -10,29 +12,63 @@ db=SQLAlchemy(app)
 migrate=Migrate(app,db)
 
 class Todo(db.Model):
-    __name__='todos'
     id = db.Column(db.Integer , primary_key=True)
     description = db.Column(db.String, nullable=False)
     completed = db.Column(db.Boolean, default=False)
+    list_id = db.Column(db.Integer , db.ForeignKey('todo_list.id'),nullable=False)
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+class TodoList(db.Model):
+    id = db.Column(db.Integer , primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    todos = db.relationship('Todo', backref='list')
 
 @app.route('/')
 def index():
-    return render_template('index.html',data=Todo.query.order_by('id').all())
+    return redirect(url_for('view_list',list_id=1))
 
-@app.route('/todos/create', methods=['POST'])
-def create_todo():
+@app.route('/lists/<list_id>')
+def view_list(list_id):
+    return render_template('index.html',
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all(),
+    lists=TodoList.query.order_by('id').all(),
+    current_list=TodoList.query.filter_by(id=list_id).order_by('id').first())
+
+@app.route('/lists/<list_id>/create', methods=['POST'])
+def create_todo(list_id):
     error=False
     data={}
     try:
         desc=request.get_json()['description']
-        item=Todo(description=desc)
+        item=Todo(description=desc,list_id=list_id)
         db.session.add(item)
         db.session.commit()
         data = jsonify({
             'description': item.description
+        })
+    except:
+        db.session.rollback()
+        error=True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    
+    if not error:
+        return data
+
+@app.route('/lists/create', methods=['POST'])
+def create_list():
+    error=False
+    data={}
+    try:
+        n=request.get_json()['name']
+        list=TodoList(name=n)
+        db.session.add(list)
+        db.session.commit()
+        data = jsonify({
+            'name': list.name,
+            'id': list.id
         })
     except:
         db.session.rollback()
